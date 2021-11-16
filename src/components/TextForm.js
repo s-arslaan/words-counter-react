584ee6f0ed4d4754b230e86a171478e927247220
s-arslaan <shaikharslaan1234@gmail.com>
2021-11-16 19:40:16 +0530

    state

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,12 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function TextForm(props) {
+  const [text, setText] = useState("Enter Text here..");
+
+  const handleUpClick = () => {
+    console.log("uppercase clicked!");
+    setText("uppercase clicked!");
+  };
+
+  const handleOnChange = (event) => {
+    console.log("on change");
+    setText(event.target.value);
+  };
+
   return (
     <div>
-      <h1>{props.heading}</h1>
-      <div class="mb-3">
-        <textarea class="form-control" id="myBox" rows="9"></textarea>
+      <h2>{props.heading}</h2>
+      <div className="mb-3">
+        <textarea
+          className="form-control"
+          value={text}
+          onChange={handleOnChange}
+          id="myBox"
+          rows="9"
+        ></textarea>
       </div>
+      <button className="btn btn-primary" onClick={handleUpClick}>
+        Convert to Uppercase
+      </button>
     </div>
   );
 }
