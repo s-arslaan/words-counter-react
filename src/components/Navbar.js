258440f6ef7e4b8edf58bc21e63282f9496aab4b
s-arslaan <shaikharslaan1234@gmail.com>
2021-11-21 00:49:16 +0530

    active

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -25,12 +25,12 @@ export default function Navbar(props) {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <Link className="nav-link active" aria-current="page" to="/">
+              <Link className={`nav-link ${props.active === 'about' ? '':'active'}`} onClick={props.activeToggle} aria-current="page" to="/">
                 Home
               </Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/about">
+              <Link className={`nav-link ${props.active === 'about' ? 'active':''}`} onClick={props.activeToggle} to="/about">
                 {props.aboutText}
               </Link>
             </li>
