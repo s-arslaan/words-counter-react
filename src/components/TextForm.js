3103f4ba42b1edbd5f3c1f5ebb2703fc55944bbe
s-arslaan <shaikharslaan1234@gmail.com>
2021-11-21 15:23:19 +0530

    final

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -4,7 +4,6 @@ export default function TextForm(props) {
   const [text, setText] = useState("");
 
   const handleUpperClick = () => {
-    // console.log("uppercase clicked!\nTEXT:: " + text);
     if (text.length>0) {
       let newText = text.trim().toUpperCase();
       setText(newText);
@@ -21,10 +20,10 @@ export default function TextForm(props) {
     }
   };
 
-  const handleCapitalizeClick = () => {
+  const handleCamelClick = () => {
     if(text.length>0) {
 
-      let arr = text.trim().split(" ");
+      let arr = text.trim().split(/\s+/);
       let newText = "";
   
       // if there is space at end of string, word[0] becomes undefined
@@ -33,15 +32,14 @@ export default function TextForm(props) {
         newText += word[0].toUpperCase() + word.slice(1).toLowerCase() + " ";
       });
       setText(newText.trim());
-      props.showAlert("Capitalized every word", "success");
+      props.showAlert("Camel Cased Sucessfully", "success");
     }
   };
   
-  const handleCapLinesClick = () => {
+  const handleCapitalizeClick = () => {
     if(text.length>0) {
 
       let arr = text.trim().split(".");
-      console.log(arr)
       let newText = "";
   
       // if there is space at end of string, word[0] becomes undefined
@@ -50,7 +48,7 @@ export default function TextForm(props) {
         newText += word.trim().charAt(0).toUpperCase() + word.slice(1).toLowerCase() + ". ";
       });
       setText(newText.trim());
-      props.showAlert("Capitalized every word", "success");
+      props.showAlert("Title Cased", "success");
     }
   };
 
@@ -69,7 +67,6 @@ export default function TextForm(props) {
   };
 
   const handleOnChange = (event) => {
-    // console.log("on change");
     setText(event.target.value);
   };
 
@@ -85,7 +82,7 @@ export default function TextForm(props) {
             <h2 className={`font-monospace text-${props.mode === "light" ? "primary" : "warning"}`}>
               {text
                   .trim()
-                  .split(" ")
+                  .split(/\s+/)
                   .filter(function (element) {
                     return element.length !== 0;
                   }).length
@@ -122,15 +119,15 @@ export default function TextForm(props) {
         </button>
         <button disabled={text.length===0}
           className="btn btn-info mx-2 my-1"
-          onClick={handleCapLinesClick}
+          onClick={handleCapitalizeClick}
         >
           <b>Capitalize</b>
         </button>
         <button disabled={text.length===0}
           className="btn btn-warning mx-2 my-1"
-          onClick={handleCapitalizeClick}
+          onClick={handleCamelClick}
         >
-          <b>Capital Each Word</b>
+          <b>Title Case</b>
         </button>
         <button disabled={text.length===0}
           className="btn btn-secondary mx-2 my-1"
@@ -145,7 +142,7 @@ export default function TextForm(props) {
         <p>
           {(text
             .trim()
-            .split(" ")
+            .split(/\s+/)
             .filter(function (element) {
               return element !== "";
             }).length * 0.008).toPrecision(3)}{" "}
